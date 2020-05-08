33b8bc8c3169dfc1924d70014f61754046cd561d
davirlein1988 <lein.davir@gmail.com>
2020-05-08 03:19:32 -0500

    added basic routing to  project

---
+++ src/router/layouts/main.vue
@@ -0,0 +1,24 @@
+<script>
+import HelloWorld from '../../components/HelloWorld.vue'
+
+export default {
+  components: { HelloWorld },
+}
+</script>
+
+<template>
+  <div :class="$style.container">
+    <HelloWorld />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '../../design';
+
+.container {
+  min-width: $size-content-width-min;
+  max-width: $size-content-width-max;
+  margin: 0 auto;
+}
+</style>
---
+++ src/router/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
