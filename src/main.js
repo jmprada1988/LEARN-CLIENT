53127fa6bff1f67f0213bcd594aa6d4bcf53e4e9
davirlein1988 <lein.davir@gmail.com>
2020-05-22 20:28:20 -0500

    Included vuetify lib to project

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store/index'
 import { router } from './router/router'
+import vuetify from './plugins/vuetify';
 
 
 new Vue({
   router,
   store,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
